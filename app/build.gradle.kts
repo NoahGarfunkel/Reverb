b0ad49692a2e45ee9aac90a6f098aa4a4cdd0e3e
Noah Garfunkel <noahsgarfunkel@gmail.com>
2026-09-07 17:04:51 -0400

    update versions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,16 +6,12 @@ plugins {
 
 android {
     namespace = "com.example.reverb"
-    compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
-    }
+    compileSdk = 37
 
     defaultConfig {
         applicationId = "com.example.reverb"
         minSdk = 28
-        targetSdk = 36
+        targetSdk = 37
         versionCode = 1
         versionName = "1.0"
 
